7b72f8dcf0fa387f79b0901cf3f5117d3a07ecf1
GranMaestroBob <BobCareyIII@gmail.com>
2013-12-15 14:52:11 -0500

    edited values

--- workspace/src/matchesinspace/MouseListener.java
+++ workspace/src/matchesinspace/MouseListener.java
@@ -22,19 +22,19 @@ public class MouseListener extends MouseInputAdapter{
      */
     public void mouseClicked (MouseEvent evt){
         int col = (evt.getX()-240)/65;
-        int row = (evt.getY()-24)/65;
+        int row = (evt.getY()-40)/65;
         
         if(col < 0){
             col = 0;
         } // if : col
-        if(col > BoardGenerator.VERTICAL_PIECES-1){
-            col = BoardGenerator.VERTICAL_PIECES-1;
+        if(col > 7){
+            col = 7;
         } // if : col
         if(row < 0){
             row = 0;
         } // if : row
-        if(row > BoardGenerator.HORIZONTAL_PIECES-1){
-            row = BoardGenerator.HORIZONTAL_PIECES-1;
+        if(row > 7){
+            row = 7;
         } // if : row
         
         gamePanel.clickPerformed(col, row);
